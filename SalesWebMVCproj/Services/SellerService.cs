57f2965e2a89fba8f468eea391e79b62266d3f23
L <100241773+jacksxx@users.noreply.github.com>
2023-03-03 14:44:31 -0300

    Details and Eager Loading

--- SalesWebMVCproj/Services/SellerService.cs
+++ SalesWebMVCproj/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using SalesWebMVCproj.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMVCproj.Services
 {
@@ -26,7 +27,9 @@ namespace SalesWebMVCproj.Services
         }
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller
+                .Include(obj => obj.Department)
+                .FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
