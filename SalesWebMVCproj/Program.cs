3f19f3b3199746c959872057a78fc7d79c634c9d
L <100241773+jacksxx@users.noreply.github.com>
2023-03-04 13:31:07 -0300

    App Locate, number and date formatting

--- SalesWebMVCproj/Program.cs
+++ SalesWebMVCproj/Program.cs
@@ -3,7 +3,10 @@ using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVCproj.Data;
 using SalesWebMVCproj.Models;
 using SalesWebMVCproj.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
+//
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Server
@@ -23,6 +26,17 @@ builder.Services.AddScoped<DepartmentService>();
 
 //application
 var app = builder.Build();
+
+//Localization
+var enUs = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUs),
+    SupportedCultures = new List<CultureInfo> { enUs },
+    SupportedUICultures = new List<CultureInfo> { enUs }
+};
+app.UseRequestLocalization(localizationOptions);
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -30,7 +44,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+//Seeds
 app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
