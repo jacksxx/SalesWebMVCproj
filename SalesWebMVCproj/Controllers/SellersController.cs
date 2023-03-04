0735f1b1fc70a0646d185dd586aa330420f28df0
L <100241773+jacksxx@users.noreply.github.com>
2023-03-04 03:04:07 -0300

    Update Seller and Service Exception

--- SalesWebMVCproj/Controllers/SellersController.cs
+++ SalesWebMVCproj/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using SalesWebMVCproj.Models;
 using SalesWebMVCproj.Models.ViewModels;
 using SalesWebMVCproj.Services;
+using SalesWebMVCproj.Services.Exception;
 
 namespace SalesWebMVCproj.Controllers
 {
@@ -73,5 +74,44 @@ namespace SalesWebMVCproj.Controllers
             return View(obj);
         }
 
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);            
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+            
+        }
     }
 }
---
+++ SalesWebMVCproj/Services/Exception/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace SalesWebMVCproj.Services.Exception
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)  { }
+    }
+}
